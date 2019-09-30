af4149957b811a756f63c89ddedb422c12e2d015
Abhishek Kainth <kainthabhishek007@gmail.com>
2019-10-01 01:22:20 +0530

    Refactor

--- public/js/xogame2.js
+++ public/js/xogame2.js
@@ -1,10 +1,10 @@
 function end(board)
 {   
-	if(board[0][0]==board[1][1]&&board[1][1]==board[2][2]&&board[0][0]!=0)return board[0][0];
+    if(board[0][0]==board[1][1]&&board[1][1]==board[2][2]&&board[0][0]!=0)return board[0][0];
     if(board[1][0]==board[1][1]&&board[1][1]==board[1][2]&&board[1][0]!=0)return board[1][0];
     if(board[2][0]==board[2][1]&&board[2][1]==board[2][2]&&board[2][0]!=0)return board[2][0];
     if(board[0][0]==board[0][1]&&board[0][1]==board[0][2]&&board[0][0]!=0)return board[0][0];
-	if(board[2][0]==board[1][1]&&board[1][1]==board[0][2]&&board[2][0]!=0)return board[2][0];
+    if(board[2][0]==board[1][1]&&board[1][1]==board[0][2]&&board[2][0]!=0)return board[2][0];
     if(board[0][1]==board[1][1]&&board[1][1]==board[2][1]&&board[0][1]!=0)return board[0][1];
     if(board[0][2]==board[1][2]&&board[1][2]==board[2][2]&&board[0][2]!=0)return board[0][2];
     if(board[0][0]==board[1][0]&&board[1][0]==board[2][0]&&board[0][0]!=0)return board[0][0];
